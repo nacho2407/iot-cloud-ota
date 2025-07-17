db0a14f1048d045c98d94a6c2da2c0fbc3a9daa7
Minjun Jang <jmj0ok@gmail.com>
2025-07-18 00:51:35 +0900

    chore: modularize driver sources into the separate project

--- embedded/main/main.cpp
+++ embedded/main/main.cpp
@@ -1,21 +1,25 @@
 #include <Arduino.h>
 
-#include "display.hpp"
-#include "tests.hpp"
+#include <driver.hpp>
+
+#include <lvgl.h>
+#include <demos/lv_demos.h>
+#include <examples/lv_examples.h>
 
 #define COFFEE_BAUD_RATE 115200
 
 extern "C" void app_main(void)
 {
     initArduino();
-    Serial.begin(COFFEE_BAUD_RATE);
+    Serial.begin((unsigned long) COFFEE_BAUD_RATE);
 
-    if(!coffee::init_lcd()) {
-        Serial.println("LCD initialization failed");
-        
-        return;
+    coffee::init_driver();
+
+    lv_demo_widgets();
+
+    while (true)
+    {
+        lv_timer_handler(); /* let the GUI do its work */
+        delay(10);
     }
-
-    // coffee::test1();
-    coffee::test2();
 }
